c8b08b905814e51949029f2d1954260d38510a3e
NimbleArchitect <56797391+NimbleArchitect@users.noreply.github.com>
2022-11-04 10:10:38 +0000

    setTimer now cancels and deletes when mSec is 0

--- globals/main.go
+++ globals/main.go
@@ -19,14 +19,26 @@ func New() *Global {
 	}
 }
 
+// SetTimer creates a timer that calls the specified function when the timer expires,
+//
+//	calling SetTime multipkle times will reset the countdown, once set the duration cant be changed
+//	if mSec is set to zero the timer is stopped without firing and deleted
 func (g *Global) SetTimer(name string, mSec int, call func()) {
 	g.lock.Lock()
 	val, ok := g.timers[name]
 	g.lock.Unlock()
 
 	if ok {
-		// exists - we have a timer setup and running, so call the reset
-		val.Reset()
+		if mSec == 0 {
+			val.Cancel()
+
+			g.lock.Lock()
+			delete(g.timers, name)
+			g.lock.Unlock()
+		} else {
+			// exists - we have a timer setup and running, so call the reset
+			val.Reset()
+		}
 	} else {
 		// not found - create a new one
 		newTimer := timer{
